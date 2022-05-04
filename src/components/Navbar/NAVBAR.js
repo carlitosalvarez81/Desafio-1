64e9c2e07feec4059d2b4a06d73c0f27461cd98b
carlos <carlos.jalvarez@hotmail.com>
2022-05-03 23:04:59 -0300

    segunda entrega navbar

---
+++ src/components/Navbar/NAVBAR.js
@@ -0,0 +1,14 @@
+
+export default function Navbar() {
+    return (
+      <header>
+          <ul>
+              <li> INICIO</li>
+              <li> NOSOTROS</li>
+              <li> PRODUCTOS</li>
+              <li> CONTACTO</li>
+          </ul>
+      </header>
+    );
+  }
+  
